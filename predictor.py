2d678131f87a71eba87cabc9180903d30d365626
Joseph Ciaravella <joseph.ciaravella@gmail.com>
2024-07-09 22:39:37 -0400

    initial downloading and viewing code

--- predictor.py
+++ predictor.py
@@ -8,3 +8,70 @@ import os
 import numpy as np
 import tensorflow as tf # This code has been tested with TensorFlow 1.6
 from sklearn.preprocessing import MinMaxScaler 
+
+
+
+def download_and_display_ticker_data(data_source, ticker):
+
+    if data_source == 'alphavantage':
+        # ====================== Loading Data from Alpha Vantage ==================================
+
+        api_key = '13DF5UCH7N8YOYCB'
+
+        # JSON file with all the stock market data for AAL from the last 20 years
+        url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
+
+        # Save data to this file
+        file_to_save = 'ticker_data/stock_market_data-%s.csv'%ticker
+
+        # If you haven't already saved data,
+        # Go ahead and grab the data from the url
+        # And store date, low, high, volume, close, open values to a Pandas DataFrame
+        if not os.path.exists(file_to_save):
+            with urllib.request.urlopen(url_string) as url:
+                data = json.loads(url.read().decode())
+                # extract stock market data
+                data = data['Time Series (Daily)']
+                df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+                for k,v in data.items():
+                    date = dt.datetime.strptime(k, '%Y-%m-%d')
+                    data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                                float(v['4. close']),float(v['1. open'])]
+                    df.loc[-1,:] = data_row
+                    df.index = df.index + 1
+            print('Data saved to : %s'%file_to_save)        
+            df.to_csv(file_to_save)
+
+
+        # If the data is already there, just load it from the CSV
+        else:
+            print('File already exists. Loading data from CSV')
+            df = pd.read_csv(file_to_save)
+
+
+    else:
+
+        # ====================== Loading Data from Kaggle ==================================
+        # You will be using HP's data. Feel free to experiment with other data.
+        # But while doing so, be careful to have a large enough dataset and also pay attention to the data normalization
+        df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+        print('Loaded data from the Kaggle repository') 
+
+
+
+    # Sort DataFrame by date
+    df = df.sort_values('Date')
+
+    # Double check the result
+    df.head()
+
+    plt.figure(figsize = (18,9))
+    plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+    plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+    plt.xlabel('Date',fontsize=18)
+    plt.ylabel('Mid Price',fontsize=18)
+    plt.show()
+
+    return df
+
+download_and_display_ticker_data("alphavantage", "AAPL")
